993c136a06f47e96d2c8181c280e2c9d2cde1d84
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-10 16:11:42 +0100

    comments exercise email

---
+++ js/script.js
@@ -0,0 +1,18 @@
+console.log("JS OK!");
+
+/* # Mail
+Crea un array di email;
+Chiedi all’utente la sua email.
+controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.*/
+
+// 1- Creare un array di email
+// 2- Chiedi all'utente la sua email (Prompt)
+// 3- Controllare se sia presente nella lista di chi può accedere
+// 4- Infine stampare un messaggio in console sull'esito
+
+
+
+/* # Gioco dei dadi
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+Stampare in console i tiri di dado e il risultato.*/
