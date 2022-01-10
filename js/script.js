257b435990677b520e100adc8dd84b94ffc66fc9
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-10 17:23:03 +0100

    added for

--- js/script.js
+++ js/script.js
@@ -1,21 +1,28 @@
 console.log("JS OK!");
 
-/* # Mail
-Crea un array di email;
-Chiedi all’utente la sua email.
-controlla che sia nella lista di chi può accedere, stampa un messaggio appropriato in console sull’esito del controllo.*/
+// # Mail
 
 // 1- Creare un array di email
 // 2- Chiedi all'utente la sua email (Prompt)
 // 3- Controllare se sia presente nella lista di chi può accedere
 // 4- Infine stampare un messaggio in console sull'esito
 
+// 1- Creare un array di email
 const email = ['marco@hotmail.com', 'andrea@hotmail.com', 'luca@hotmail.com', 'luigi@hotmail.com'];
+// 2- Chiedi all'utente la sua email (Prompt)
+const askEmail = prompt('Inserisci la tua E-mail', 'marco@hotmail.com');
 
-const askEmail = prompt('Inserisci la tua e-mail', 'marco@hotmail.com');
-console.log(askEmail);
+let emailCheck = '';
+// 3- Controllare se sia presente nella lista di chi può accedere
+for (let i = 0; i < email.length; i++) {
+    if (email[i] === askEmail) {
+        emailCheck = `Benvenuto ${askEmail}`;
+        console.log(emailCheck);
+    }
+}
 
-/* # Gioco dei dadi
-Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
-Stabilire il vincitore, in base a chi fa il punteggio più alto.
-Stampare in console i tiri di dado e il risultato.*/
+// # Gioco dei dadi
+
+// 1- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+// 2- Stabilire in vincitore per il punteggio piu' altro
+// 3- Stampare in console i tiri dei dadi e il risultato
