23173315ff36e4e9157ae4d23a53f0ad99f23427
MarcoSalvatore91 <marcosalvatore91@hotmail.com>
2022-01-10 18:03:11 +0100

    added new script

---
+++ js/script2.js
@@ -0,0 +1,29 @@
+console.log("JS OK!")
+// # Gioco dei dadi
+
+// 1- Generare un numero random da 1 a 6, sia per il giocatore sia per il computer
+// 2- Stabilire in vincitore per il punteggio piu' alto
+// 3- Stampare in console i tiri dei dadi e il risultato
+
+// 1a- Genera numero random per utente
+const userNumber = Math.floor(Math.random() * 6) + 1;
+// 1b- Genera numero random per il computer
+const cpuNumber = Math.floor(Math.random() * 6) + 1;
+
+// 2- Stabilire in vincitore per il punteggio piu' alto
+
+let winner = '';
+
+if (userNumber > cpuNumber) {
+    winner = `Hai Vinto!!!`
+} else if (userNumber < cpuNumber) {
+    winner = `Mi dispiace, hai perso`
+} else {
+    winner = `Hai pareggiato, ritenta!`
+}
+
+// 3- Stampare in console i tiri dei dadi e il risultato
+
+console.log(userNumber); 
+console.log(cpuNumber);
+console.log(winner);
